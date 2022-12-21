27e4c60dc3a24409a5c8cc067491f00401608f30
Luke-Xiao-16 <117942187+Luke-Xiao-16@users.noreply.github.com>
2022-12-21 13:52:31 -0500

    Add files via upload
    2022-12-21 First commit, diver class

--- MyWorld.java
+++ MyWorld.java
@@ -2,8 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
- * 
- * @author (your name) 
+ * Under the sea ~~~
+ * @author (Luke) 
  * @version (a version number or a date)
  */
 public class MyWorld extends World
@@ -16,6 +16,6 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1); 
     }
 }
