d4f13348b0e9d63495facae8522887634ea969b5
Luke-Xiao-16 <350436366@gapps.yrdsb.ca>
2023-01-13 14:14:12 -0500

    title screen
    title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -35,22 +35,22 @@ public class TitleScreen extends World
         //Diver diver = new Diver();
         Treasure treasure = new Treasure();
         addObject(treasure,300,200);
-        treasure.setLocation(300,235);//235
+        treasure.setLocation(750,520);//235
         
         Label label1 = new Label("Control The Diver",35);
         addObject(label1,300,130);
-        label1.setLocation(300,130);
+        label1.setLocation(300,410);
         
         Label label2 = new Label("Reach The Treasure!",35);
         addObject(label2,300,160);
-        label2.setLocation(300,165);
+        label2.setLocation(300,450);
         
         Label label3 = new Label("Press <Space> To Start",35);
         addObject(label3,300,300);
-        label3.setLocation(300,300);
+        label3.setLocation(300,500);
         
-        Label label4 = new Label("Use W,A,S,D To Move", 35);
+        Label label4 = new Label("Use Arrow Keys Or W,A,S,D To Move", 35);
         addObject(label4,300,370);
-        label4.setLocation(300,360);
+        label4.setLocation(300,550);
     }
 }
